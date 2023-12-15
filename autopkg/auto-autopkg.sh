45e62a3e1f6f4a9776c7a57b5956e1439766ac57
Dave Bergstrom <dwbergstrom@gmail.com>
2023-12-14 21:30:28 -0500

    add autopkg script

--- autopkg/auto-autopkg.sh
+++ autopkg/auto-autopkg.sh
@@ -5,7 +5,7 @@ OVERRIDES_DIR="/Users/dwbergstrom/git/munki_files/autopkg/overrides"
 
 # Run autopkg for each override in the directory
 for override in "$OVERRIDES_DIR"/*; do
-  autopkg run "$override"
+  autopkg run -v "$override"
 done
 
 # Get the current date
@@ -14,7 +14,7 @@ current_date=$(date +%Y%m%d)
 # Check for new overrides and run add-pkg for each
 while IFS= read -r new_override; do
   installer_name=$(xmllint --xpath 'string(//key[.="NAME"]/following-sibling::string[1])' "$new_override")
-  add-pkg "$installer_name" --manifest site_default --section managed_updates
+  manifestutil add-pkg "$installer_name" --manifest site_default --section managed_updates
 done < <(find "$OVERRIDES_DIR" -type f -newermt "$current_date")
 
 # Run makecatalogs
