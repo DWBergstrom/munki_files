1eac6b560115410acec9761dff5a71848e3bab6a
Dave Bergstrom <dwbergstrom@Daves-Mac-mini.localdomain>
2024-08-10 16:30:28 -0400

    2024-08-10 16:07:24 Updating munki

--- autopkg/auto-autopkg.sh
+++ autopkg/auto-autopkg.sh
@@ -10,10 +10,10 @@ fi
 COMMIT_DATE=$(date "+%Y-%m-%d %H:%M:%S")
 
 # Define the directory containing the overrides
-OVERRIDES_DIR="/Users/dwbergstrom/git/munki_files/autopkg/overrides"
+OVERRIDES_DIR="/Users/dwbergstrom/github/munki_files/autopkg/overrides"
 
 # Define Munki repo path
-MUNKI_REPO_PATH="/Users/dwbergstrom/git/munki_files/munki_web/munki_repo"
+MUNKI_REPO_PATH="/Users/dwbergstrom/github/munki_files/munki_web/munki_repo"
 
 # Clean up old apps
 # rm -Rf "${MUNKI_REPO_PATH}/pkgs/"*
@@ -37,8 +37,8 @@ echo "Running makecatalogs..."
 makecatalogs --skip-pkg-check "$MUNKI_REPO_PATH"
 
 # Serve munki repo via tailscale
-echo "setting up tailscale funnel..."
-tailscale funnel /Users/dwbergstrom/git/munki_files/munki_web/munki_repo&
+# echo "setting up tailscale funnel..."
+# /Users/dwbergstrom/git/munki_files/munki_web/munki_repo&
 
 # Save changes to git
 git add --all
